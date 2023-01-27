c1d91c57f252ae860fbcd476e29eb2186c8223a4
Michael Wagner <michaelwagner780@gmail.com>
2023-01-27 12:20:08 -0500

    additional heading styles

--- client/src/components/Heading.tsx
+++ client/src/components/Heading.tsx
@@ -19,50 +19,31 @@ const Container = styled.div`
 		position: relative;
 		right: 3px;
 		top: 9px;
-
-		/* animation-name: title;
-		animation-duration: 0.5s;
-		animation-timing-function: ease-in;
-		animation-fill-mode: forwards; */
 	}
 
 	h2 {
 		font-size: var(--h3);
-		color: var(--accent-900);
 
 		&::before {
-			content: 'Montréal Based';
+			content: 'Montréal based';
 			color: var(--primary-fg-300);
 			position: absolute;
 			font-size: var(--h5);
+			font-weight: lighter;
+			font-style: italic;
 			bottom: 50px;
+			left: 13px;
 		}
 
 		animation-name: subtitle;
-		animation-duration: 1s;
+		animation-duration: 0.6s;
 		animation-timing-function: ease-in;
 		animation-fill-mode: forwards;
-	}
-
-	@keyframes title {
-		from {
-			opacity: 0;
-		}
-
-		to {
-			opacity: 1;
-		}
 	}
 
 	@keyframes subtitle {
 		0% {
 			color: var(--primary-fg-300);
-			/* opacity: 0; */
-		}
-
-		50% {
-			color: var(--primary-fg-300);
-			/* opacity: 1; */
 		}
 
 		99% {
@@ -79,18 +60,18 @@ const Logo = styled.img`
 	position: absolute;
 	z-index: -1;
 	animation-name: logo;
-	animation-duration: 1s;
-	animation-timing-function: ease-in-out;
+	animation-duration: 0.6s;
+	animation-timing-function: ease-out;
 	animation-fill-mode: forwards;
 
 	@keyframes logo {
 		from {
-			width: 160px;
+			width: 180px;
 			filter: grayscale(10) invert(0.2);
 			opacity: 0;
 			bottom: 22px;
 			right: -9px;
-			rotate: 270deg;
+			rotate: 180deg;
 		}
 
 		to {
