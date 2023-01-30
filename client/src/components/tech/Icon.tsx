b367aff2fdbdc8e36a6f57dd14582f0c48a8f23d
Michael Wagner <michaelwagner780@gmail.com>
2023-01-30 15:18:29 -0500

    add technologies section

---
+++ client/src/components/tech/Icon.tsx
@@ -0,0 +1,110 @@
+import styled from 'styled-components'
+
+type Props = {
+	color: string
+	text: string
+	textRotation: string
+	svg: string[]
+}
+
+const SIZE = '150px'
+
+function Icon({ color, text, textRotation, svg }: Props) {
+	return (
+		<SVG
+			width="200"
+			height="200"
+			viewBox="0 -50 200 250"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg">
+			<defs>
+				<path
+					d="M15.555599999999998,100A84.4444,84.4444 0 1 1184.4444,100A84.4444,84.4444 0 1"
+					id="textcurve"
+				/>
+
+				<filter
+					id="glow"
+					filterUnits="userSpaceOnUse"
+					x="-50%"
+					y="-50%"
+					width="200%"
+					height="200%">
+					<feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur5" />
+					<feGaussianBlur
+						in="SourceGraphic"
+						stdDeviation="10"
+						result="blur10"
+					/>
+					<feGaussianBlur
+						in="SourceGraphic"
+						stdDeviation="20"
+						result="blur20"
+					/>
+					<feGaussianBlur
+						in="SourceGraphic"
+						stdDeviation="30"
+						result="blur30"
+					/>
+					<feGaussianBlur
+						in="SourceGraphic"
+						stdDeviation="50"
+						result="blur50"
+					/>
+					<feMerge result="blur-merged">
+						<feMergeNode in="blur10" />
+						<feMergeNode in="blur20" />
+						<feMergeNode in="blur30" />
+						<feMergeNode in="blur50" />
+					</feMerge>
+					<feColorMatrix
+						result="blur"
+						in="blur-merged"
+						type="matrix"
+						values="0 0 0 0 0
+                             0 0 0 0 0
+                             0 0 0 0 0
+                             0 0 0 1 0"
+					/>
+					<feMerge>
+						<feMergeNode in="blur" />
+						<feMergeNode in="blur5" />
+						<feMergeNode in="SourceGraphic" />
+					</feMerge>
+				</filter>
+			</defs>
+
+			<text dx={textRotation}>
+				<textPath xlinkHref="#textcurve" fill={color}>
+					{text}
+				</textPath>
+			</text>
+
+			<circle cx="100" cy="100" r="78.8889" stroke={color} />
+
+			{svg.map((path, index) => (
+				<path d={path} key={index} stroke={color} />
+			))}
+		</SVG>
+	)
+}
+
+const SVG = styled.svg`
+	width: ${SIZE};
+	height: ${SIZE};
+
+	text {
+		/* font-size: 1.4rem; */
+		font-size: 1.06rem;
+		font-family: Arial, Helvetica, sans-serif;
+		letter-spacing: 5px;
+	}
+
+	path,
+	circle,
+	text {
+		/* filter: url(#glow); */
+	}
+`
+
+export default Icon
---
+++ client/src/components/tech/Tech.tsx
@@ -0,0 +1,102 @@
+import styled from 'styled-components'
+import bg from '/scuffed.jpg'
+import Icon from './Icon'
+import Icons from './Icons'
+
+function Tech() {
+	return (
+		<Background>
+			<Container>
+				<Heading>
+					<h2>Technologies</h2>
+					<h3>
+						<span className="a">Languages </span>
+						<span className="b">Frameworks </span>
+						<span className="c">Tools </span>
+					</h3>
+				</Heading>
+				<IconContainer>
+					{Icons.map((icon) => (
+						<Icon
+							key={icon.text}
+							color={icon.color}
+							text={icon.text}
+							textRotation={icon.textRotation}
+							svg={icon.svg}
+						/>
+					))}
+				</IconContainer>
+			</Container>
+		</Background>
+	)
+}
+
+const Background = styled.section`
+	min-height: fit-content;
+	/* height: 126vh; */
+	width: 100vw;
+	background: linear-gradient(rgb(26, 27, 38, 1), rgba(0, 0, 0, 0.4)),
+		url(${bg});
+	background-size: cover;
+	opacity: 0;
+	animation-name: fadein;
+	animation-duration: 1s;
+	animation-timing-function: linear;
+	animation-fill-mode: forwards;
+
+	/* clip-path: polygon(0% 0%, 0% 90%, 100% 100%, 100% 0%); */
+
+	@keyframes fadein {
+		from {
+			opacity: 0;
+		}
+
+		to {
+			opacity: 1;
+		}
+	}
+`
+
+const Container = styled.div`
+	display: grid;
+	place-content: center;
+	gap: 60px;
+	max-width: 1200px;
+	margin: auto;
+	padding: 40vh 20px 25vh;
+`
+
+const Heading = styled.div`
+	text-align: center;
+
+	h2 {
+		color: var(--primary-fg-300);
+	}
+
+	h3 {
+		margin-top: -30px;
+	}
+
+	span {
+		font-size: var(--p);
+		font-weight: lighter;
+		&.a {
+			color: var(--accent-900);
+		}
+		&.b {
+			color: var(--accent-800);
+		}
+		&.c {
+			color: var(--accent-1000);
+		}
+	}
+`
+
+const IconContainer = styled.div`
+	width: fit-content;
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-around;
+`
+
+export default Tech
